e7bdc8cfbfb15b98676b5abf98597a073437b475
Dante Pitrella <dantepitrella1@yahoo.com>
2024-07-04 17:01:51 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const banner = document.getElementById("img");
+let courrentSlide = 0
+let tagLineContent = document.getElementById("tagLine")
+const dots = document.getElementById("dots")
+
+//slides ARROWS
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener("click", () => {
+	courrentSlide --
+	if (courrentSlide <= 0) {
+		courrentSlide = slides.length -1
+	} 
+	tagLineContent.innerHTML = slides [courrentSlide].tagLine
+	banner.src = "assets/images/slideshow/" + slides [courrentSlide].image
+	updateDots();
+	
+})
+arrowRight.addEventListener("click", () => {
+	courrentSlide ++
+	if (courrentSlide >= slides.length) {
+		courrentSlide = 0
+	} 
+	tagLineContent.innerHTML = slides [courrentSlide].tagLine
+	banner.src = "assets/images/slideshow/" + slides [courrentSlide].image
+	updateDots();
+	
+})
+
+
+//DOTS
+
+for (let i = 0; i < slides.length; i++) {
+const dot = document.createElement("span")
+	dot.classList.add("dot")
+	dots.appendChild(dot)
+	dot.addEventListener("click", () => {
+		courrentSlide = i
+		banner.src = slides[courrentSlide].image;
+		Text.innerHTML = slides[courrentSlide].tagLine
+		updateDots()
+	})
+}
+
+let dot = document.querySelectorAll(".dot");
+dot[0].classList.add("dot_selected");
+
+function updateDots() {
+	for (let i = 0; i <dot.length; i++){
+		if (i === courrentSlide) {
+			dot[i].classList.add("dot_selected");
+		}else {
+			dot[i].classList.remove("dot_selected")
+		}
+	}
+}
